f588e6b254226aecf76934273bb3a4739b656326
Igor Kuryakov <igor.kuryakov@hpe.com>
2023-08-12 17:30:27 +0500

    feat(): Add simulation api

---
+++ src/utils/emulateApi.ts
@@ -0,0 +1,11 @@
+function simulateApiCall() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const responseData = { message: "API call successful" };
+        resolve(responseData);
+      }, 3000);
+    });
+  }
+
+
+export default simulateApiCall;
